7c33b64c2f75f908010c90a4e4ae6b635999f500
Guiozo@Guilher,e <guilherme.mcr06@outlook.com>
2025-09-08 12:52:39 -0300

    Create Escalonador_de_Processos.java

---
+++ Escalonador_de_Processos.java
@@ -0,0 +1,49 @@
+class Escalonador_de_Processos {
+    private int pro = 882, pro2 = 884, pro3 = 888, altapriori;  
+            private int pro4 = 862, pro5 = 864, pro6 = 868, baixapriori; 
+            private int pro7 = 832, pro8 = 834, pro9 = 836, mediapriori; 
+            private char recurso_necessario, ciclos_necessarios;
+
+        public Escalonador_de_Processos(String[] args) {
+            private int pro = 882, pro2 = 884, pro3 = 888, altapriori;  
+            
+
+            altapriori = (pro = 882, pro2 = 884, pro3 = 888);
+            baixapriori = (pro4 = 862, pro5 = 864, pro6 = 868);
+            mediapriori = (pro7 = 832, pro8 = 834, pro9 = 836);
+        }
+
+
+            this.altapriori = altapriori;
+            this.baixapriori = baixapriori;
+            this.mediapriori =  mediapriori();
+        
+
+            public int mediapriori() {
+                return mediapriori;
+            }
+
+            public int getbaixapriori() {
+                return baixapriori;
+            }
+
+            public int getmediapriori() {
+                return mediapriori;
+            }
+
+
+
+        }
+
+
+
+            
+
+
+
+
+
+
+
+        
+
